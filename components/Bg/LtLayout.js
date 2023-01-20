04ac671a28477c344f946cc85415f5b303a92081
Gragolha <luka.100.nerozi@gmail.com>
2023-01-20 03:13:46 -0300

    size

--- components/Bg/LtLayout.js
+++ components/Bg/LtLayout.js
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 
 export default function Layout({ children }) {
   return (
-    <Box bgColor="#151515" h="100vw">
+    <Box bgColor="#151515" h="5000px">
       <Navbar />
       {children}
     </Box>
